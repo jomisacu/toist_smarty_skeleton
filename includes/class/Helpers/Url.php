b8dc58de982be81d778d7ae6e56c94f6389e512a
Jorge <jomisacu.software@gmail.com>
2019-01-10 09:55:18 -0400

    allow multiple params on image url generation

--- includes/class/Helpers/Url.php
+++ includes/class/Helpers/Url.php
@@ -6,6 +6,15 @@ class Url
 {
     public function image_url($data)
     {
+    	if (!is_array($data)) {
+    		$values = func_get_args();
+    		$aux = [];
+			foreach (['id', 'w', 'h', 'mw', 'mh'] as $index => $param) {
+				$aux[$param] = $values[$index];
+    		}
+			$data = $aux;
+		}
+    	
         return 'image.php?'.http_build_query($data);
     }
     
