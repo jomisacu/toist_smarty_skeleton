cbf5a344707d0ca49ce802ebb31cba7d61dbc813
Jorge <jomisacu.software@gmail.com>
2019-07-21 23:13:41 -0400

    set as default content the content #1

--- content.php
+++ content.php
@@ -5,7 +5,7 @@ require __DIR__ . '/includes/init.php';
 require __DIR__ . '/includes/site_menus.php';
 require __DIR__ . '/includes/site_contents.php';
 
-$contentid = $request->query->getInt('id');
+$contentid = $request->query->getInt('id', 1);
 
 $content = toist_getContentLists()['latest'][$contentid];
 
